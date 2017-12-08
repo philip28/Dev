9f67292d74bffa30cf5140cc33b2e7ebe52280b6
Philipp Maksakov <philip_maksakov@hotmail.com>
2017-12-08 15:55:04 +0300

    v 1.1

--- detect/detect/detect.cpp
+++ detect/detect/detect.cpp
@@ -10,10 +10,18 @@ using namespace cv;
 
 namespace fs = std::experimental::filesystem;
 
-int detect(const fs::path& image_file, const ConfigReader& config)
+typedef struct {
+	fs::path image;
+	Rect area;
+	int level;
+	double weight;
+} detect_stat;
+
+typedef vector<detect_stat> detect_vec;
+
+int detect(const fs::path& image_file, const ConfigReader& config, detect_vec& detected)
 {
-	string cascade_file, output_dir;
-	string action = "outline";
+	string cascade_file;
 	int image_resize_width = 0, minNeighbours = 3, minSize = 0, maxSize = 0;
 	double scaleFactor = 1.1;
 
@@ -23,8 +31,6 @@ int detect(const fs::path& image_file, const ConfigReader& config)
 	config.GetParamValue("cc_minNeighbours", minNeighbours);
 	config.GetParamValue("cc_scaleFactor", scaleFactor);
 	config.GetParamValue("image_resize_width", image_resize_width);
-	config.GetParamValue("output_dir", output_dir);
-	config.GetParamValue("action", action);
 
 	Mat image_src = imread(image_file.string(), IMREAD_COLOR);
 	if (image_src.empty()) throw;
@@ -39,38 +45,108 @@ int detect(const fs::path& image_file, const ConfigReader& config)
 	cvtColor(image_src, image_grey, COLOR_BGR2GRAY);
 
 	vector<Rect> detected_areas;
+	vector<int> reject_levels;
+	vector<double> level_weights;
+	detect_stat stat;
 	CascadeClassifier cascade(cascade_file);
 	Size min(minSize, minSize), max(maxSize, maxSize);
 
-	cascade.detectMultiScale(image_grey, detected_areas, scaleFactor, minNeighbours, 0, min, max);
+	cascade.detectMultiScale(image_grey, detected_areas, reject_levels, level_weights, scaleFactor, minNeighbours, 0, min, max, true);
+
+	int i;
+	for (i = 0; i < detected_areas.size(); i++)
+	{
+		stat.image = image_file;
+		stat.area = detected_areas[i];
+		stat.level = reject_levels[i];
+		stat.weight = level_weights[i];
+		detected.push_back(stat);
+	}
+
+	return i;
+}
+
+int output(const ConfigReader& config, detect_vec& detected, bool verbose = true)
+{
+	string output_dir;
+	string action = "outline";
+	int crop_num = 0;
+
+	config.GetParamValue("output_dir", output_dir);
+	config.GetParamValue("action", action);
+	config.GetParamValue("crop_num", crop_num);
 
 	if (output_dir[output_dir.length() - 1] == '\\') output_dir.erase(output_dir.length() - 1, 1);
 
-	if (action == "outline")
+	fs::create_directory(output_dir);
+
+	if (action == "crop_min_weight")
 	{
-		for (int i = 0; i < detected_areas.size(); i++)
+		sort(detected.begin(), detected.end(), [](detect_stat a, detect_stat b) {
+			return a.weight < b.weight;
+		});
+
+		if (crop_num < detected.size())
 		{
-			rectangle(image_src, detected_areas[i], Scalar(255, 0, 255), 2, LINE_4);
-		}
-		if (detected_areas.size())
-		{
-			string output_file = output_dir + '\\' + image_file.stem().string() + "_outline" + image_file.extension().string();
-			imwrite(output_file, image_src);
+			detected.erase(detected.begin() + crop_num, detected.end());
 		}
 	}
-	else if (action == "crop")
+	else if (action == "crop_max_weight")
 	{
-		for (int i = 0; i < detected_areas.size(); i++)
+		sort(detected.begin(), detected.end(), [](detect_stat a, detect_stat b) {
+			return a.weight > b.weight;
+		});
+
+		if (crop_num < detected.size())
 		{
-			Mat crop = image_src(detected_areas[i]);
-			char buf[4];
-			sprintf_s(buf, 4, "%03d", i);
-			string output_file = output_dir + '\\' + image_file.stem().string() + "_crop" + buf + image_file.extension().string();
+			detected.erase(detected.begin() + crop_num, detected.end());
+		}
+	}
+
+	string curimage;
+	Mat image_src;
+	int i;
+	for (i = 0; i < detected.size(); i++)
+	{
+		if (detected[i].image.string() != curimage)
+		{
+			curimage = detected[i].image.string();
+			image_src = imread(curimage, IMREAD_COLOR);
+			if (image_src.empty()) throw;
+		}
+
+		if (action == "outline")
+		{
+			rectangle(image_src, detected[i].area, Scalar(255, 0, 255), 2, LINE_4);
+			char notes[50];
+			sprintf_s(notes, "%d, %.2f", detected[i].level, detected[i].weight);
+			putText(image_src, notes, Point(detected[i].area.x + 5, detected[i].area.y + 10), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(255, 255, 255), 1);
+
+			if (i + 1 == detected.size() || detected[i + 1].image.string() != curimage)
+			{
+				string output_file = output_dir + '\\' + detected[i].image.stem().string() + "_outline" + detected[i].image.extension().string();
+
+				if (verbose)
+					printf("Writing %s...\n", output_file.c_str());
+
+				imwrite(output_file, image_src);
+			}
+		}
+		else if (action == "crop" || action == "crop_min_weight" || action == "crop_max_weight")
+		{
+			Mat crop = image_src(detected[i].area);
+			char buf[12];
+			sprintf_s(buf, 12, "%04d_%.2f", i, detected[i].weight);
+			string output_file = output_dir + '\\' + detected[i].image.stem().string() + "_crop" + buf + detected[i].image.extension().string();
+
+			if (verbose)
+				printf("Writing %s...\n", output_file.c_str());
+
 			imwrite(output_file, crop);
 		}
 	}
 
-	return (int)detected_areas.size();
+	return i;
 }
 
 int main(int argc, char* argv[])
@@ -83,9 +159,10 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	string input_dir, input_file;
+	string input_dir, input_file, input_type;
 	config.GetParamValue("input_dir", input_dir);
 	config.GetParamValue("input_file", input_file);
+	config.GetParamValue("input_type", input_type);
 
 	vector<fs::path> input_files;
 	if (input_file.size())
@@ -96,15 +173,35 @@ int main(int argc, char* argv[])
 	{
 		fs::directory_iterator begin(input_dir), end;
 		copy_if(begin, end, std::back_inserter(input_files), [](const fs::path& path) {
-			return fs::is_regular_file(path) && (path.extension() == ".jpg");
+			return fs::is_regular_file(path) && (path.extension() == ".jpg" || path.extension() == ".JPG");
 		});
 	}
 
-	vector<fs::path>::iterator it = input_files.begin();
-	for (int i = 0; it != input_files.end(); i++, it++)
+	srand((int)time(NULL));
+	detect_vec detected;
+
+	if (input_type == "random")
 	{
-		printf("#%d %s: %d detected\n", i, (*it).string().c_str(), detect((*it).string(), config));
+		int i = 0;
+		while (i < input_files.size())
+		{
+			size_t f = (int)((double)rand() / RAND_MAX * (input_files.size() - 1));
+			printf("#%d %s detecting: ", i, input_files[f].string().c_str());
+			printf("%d detected\n", detect(input_files[f].string(), config, detected));
+			i++;
+		}
+	}
+	else
+	{
+		vector<fs::path>::iterator it = input_files.begin();
+		for (int i = 0; it != input_files.end(); i++, it++)
+		{
+			printf("#%d %s detecting: ", i, (*it).string().c_str());
+			printf("%d detected\n", detect((*it).string(), config, detected));
+		}
 	}
 
-	return 0;
+	int d = output(config, detected);
+
+	return d;
 }
